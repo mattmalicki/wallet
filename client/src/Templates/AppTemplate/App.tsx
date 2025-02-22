73eff4bcad9929a74285ed1a4ef19b2b9b7f3591
mattmalicki <mateusz.malicki96@gmail.com>
2025-02-22 14:33:05 +0100

    Starting project.

---
+++ client/src/Templates/AppTemplate/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <button>TEXT</button>
+    </div>
+  );
+};
+
+export default App;
