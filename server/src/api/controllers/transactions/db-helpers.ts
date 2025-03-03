43bb2647384216e55f3aa3203e8adfb98783dce5
mattmalicki <mateusz.malicki96@gmail.com>
2025-03-03 16:53:50 +0100

    Creating methods to comunicate with database.

---
+++ server/src/api/controllers/transactions/db-helpers.ts
@@ -0,0 +1,68 @@
+import { Types } from "mongoose";
+import {
+  Transaction,
+  ITransaction,
+  TransactionStatus,
+} from "../../../models/transaction";
+
+async function addTransaction(transaction: ITransaction) {
+  const newTransaction = new Transaction(transaction);
+  return newTransaction;
+}
+
+async function getTransactions(userId: string) {
+  const transactions = await Transaction.find({
+    userId: new Types.ObjectId(userId),
+  });
+  return transactions;
+}
+
+async function updateTransaction(
+  userId: string,
+  id: string,
+  to?: string,
+  value?: number,
+  currency?: string,
+  status?: string
+) {
+  const transaction = await Transaction.findOne({
+    _id: new Types.ObjectId(id),
+    userId: new Types.ObjectId(userId),
+  });
+  if (!transaction) {
+    throw Error("Unauthorized.");
+  }
+  if (to) {
+    transaction.to = to;
+  }
+  if (value) {
+    transaction.value = value;
+  }
+  if (currency) {
+    transaction.currency = currency;
+  }
+  if (status) {
+    if (status.toLowerCase() === "pending")
+      transaction.status = TransactionStatus.pending;
+    if (status.toLowerCase() === "approved")
+      transaction.status = TransactionStatus.approved;
+    if (status.toLowerCase() === "completed")
+      transaction.status = TransactionStatus.completed;
+  }
+  transaction.save();
+  return transaction;
+}
+
+async function deleteTransaction(userId: string, id: string) {
+  await Transaction.findOneAndDelete({
+    _id: new Types.ObjectId(id),
+    userId: new Types.ObjectId(userId),
+  });
+}
+
+export {
+  addTransaction,
+  getTransactions,
+  updateTransaction,
+  deleteTransaction,
+};
