ee3caae1e29ee7889b44339f4e300166f1a65ff1
mattmalicki <mateusz.malicki96@gmail.com>
2025-03-25 16:16:53 +0100

    e

---
+++ client/src/redux/store.tsx
@@ -0,0 +1,12 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage/index";
+import { configureStore } from "@reduxjs/toolkit";
