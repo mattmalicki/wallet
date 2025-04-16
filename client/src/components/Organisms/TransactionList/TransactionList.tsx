63af92844f518567fcff8229d1a265a8eb789f45
mattmalicki <mateusz.malicki96@gmail.com>
2025-04-16 23:01:44 +0200

    Styling Transaction list.

--- client/src/components/Organisms/TransactionList/TransactionList.tsx
+++ client/src/components/Organisms/TransactionList/TransactionList.tsx
@@ -1,6 +1,7 @@
 import { FC, MouseEventHandler } from "react";
 import { TransactionItem } from "../TransactionItem/TransactionItem";
 import { useTransactions } from "../../../hooks/useTransactions";
+import styles from "./TransactionList.module.css";
 
 interface TransactionListProp {
   editHandler: MouseEventHandler<HTMLButtonElement>;
@@ -10,7 +11,7 @@ const TransactionList: FC<TransactionListProp> = (props) => {
   const { transactions } = useTransactions();
 
   return (
-    <ul>
+    <ul className={styles.transactionList}>
       {transactions.map((transaction) => (
         <TransactionItem
           key={transaction._id}
