7cfdaabb70c1472b9abb1129b9a100af21ec670a
mattmalicki <mateusz.malicki96@gmail.com>
2025-04-26 21:58:36 +0200

    PageButtons change pages and changes icons based on a location.

--- client/src/components/Molecules/PageButtons/PageButtons.tsx
+++ client/src/components/Molecules/PageButtons/PageButtons.tsx
@@ -1,13 +1,27 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./PageButtons.module.css";
 import { IconButton } from "../../Atoms/IconButton/IconButton";
+import { Link, useLocation } from "react-router-dom";
 
 const PageButtons: FC = () => {
+  const [current, setCurrent] = useState<string>("/home");
+  const location = useLocation();
+
+  useEffect(() => {
+    setCurrent(location.pathname);
+  }, [location]);
+
   return (
     <div className={styles.pageButtons}>
-      <IconButton name="home" isCurrent={true} />
-      <IconButton name="statistics" />
-      <IconButton name="currency" />
+      <Link to="/home">
+        <IconButton name="home" isCurrent={current === "/home"} />
+      </Link>
+      <Link to="/statistics">
+        <IconButton name="statistics" isCurrent={current === "/statistics"} />
+      </Link>
+      <Link to="/currency">
+        <IconButton name="currency" isCurrent={current === "/currency"} />
+      </Link>
     </div>
   );
 };
